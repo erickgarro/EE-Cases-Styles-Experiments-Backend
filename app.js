3dc8c08fd93635776b04bafdb1cb9cf60ee1319e
erickgarro <erickgarro@gmail.com>
2022-05-01 03:30:32 +0200

    File reorganization

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-const questionsRouter = require('./routes/questions');
-const responsesRouter = require('./routes/responses');
-const usersRouter = require('./routes/users');
-
 const app = express();
+const PORT = process.env.PORT || 8080;
+
+const indexRouter = require('./src/routes');
+const questionsRouter = require('./src/routes/questions');
+const responsesRouter = require('./src/routes/responses');
+const usersRouter = require('./src/routes/users');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,5 +21,9 @@ app.use('/', indexRouter);
 app.use('/questions', questionsRouter);
 app.use('/responses', responsesRouter);
 app.use('/users', usersRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
