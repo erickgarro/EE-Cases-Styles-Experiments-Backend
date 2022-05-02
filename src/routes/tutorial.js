0aa7a87d088f770f050ccf09ff500d0b43e8c087
erickgarro <erickgarro@gmail.com>
2022-05-02 12:49:01 +0200

    Added tutorial questions generation and route

---
+++ src/routes/tutorial.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const Questions = require('../questions/Questions');
+
+
+/*
+ * GET /questions/get/:userId/ Given a user id, find a pre-existing questions file and return it, else create a new one and return it.
+ */
+router.get('/get/:userId', function(req, res, next) {
+  const userId = req.params.userId;
+  let questions;
+  try {
+    questions = Questions.createQuestions(userId);
+    res.status(200).json(questions);
+    console.log(`Tutorial questions for user ${userId} generated.`);
+  } catch (err) {
+    console.error(`Something when wrong when generating tutorial questions for user ${userId}`);
+    res.status(500).json({error: err});
+  }
+  res.send();
+});
+
+module.exports = router;
