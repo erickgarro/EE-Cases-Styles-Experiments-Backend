710ed9115c1bd75385843a44477ee6e898a35d11
erickgarro <erickgarro@gmail.com>
2022-05-07 14:46:29 +0200

    Refactored reports

---
+++ src/questions/questions_specs.js
@@ -0,0 +1,205 @@
+let words_specs = {
+  1: {
+    'words': ['telotaxi', 'is', 'sour'],
+    'caseStyle': 'k',
+    'color': 'm'
+  },
+  2: {
+    'words': ['vinyl', 'paint'],
+    'caseStyle': 'k',
+    'color': 'm'
+  }.
+  3: {
+    'words': ['handy', 'dandy'],
+    'caseStyle': 'k',
+    'color': 'm'
+  },
+  4: {
+    'words': ['file', 'not', 'found'],
+    'caseStyle': 'k',
+    'color': 'm'
+  },
+  5: {
+    'words': ['brew', 'coffee', 'now'],
+    'caseStyle': 'k',
+    'color': 'm'
+  }
+  6: {
+    'words': ['asphyxy', 'cloud'],
+    'caseStyle': 'k',
+    'color': 'c'
+  },
+  7: {
+    'words': ['narrow', 'polyphone'],
+    'caseStyle': 'k',
+    'color': 'c'
+  },
+  8: {
+    'words': ['dark', 'side', 'guy'],
+    'caseStyle': 'k',
+    'color': 'c'
+  },
+  9: {
+    'words': ['biotron', 'mnemonic'],
+    'caseStyle': 'k',
+    'color': 'c'
+  },
+  10: {
+    'words': ['modal', 'horizontal', 'size'],
+    'caseStyle': 'k',
+    'color': 'c'
+  },
+  11: {
+    'words': ['sad', 'user'],
+    'caseStyle': 'c',
+    'color': 'm'
+  },
+  12: {
+    'words': ['come', 'closer'],
+    'caseStyle': 'c',
+    'color': 'm'
+  },
+  13: {
+    'words': ['rainbow', 'kid'],
+    'caseStyle': 'c',
+    'color': 'm'
+  },
+  14: {
+    'words': ['has', 'golden', 'pass'],
+    'caseStyle': 'c',
+    'color': 'm'
+  },
+  15: {
+    'words': ['megatron', 'movies'],
+    'caseStyle': 'c',
+    'color': 'm'
+  },
+  16: {
+    'words': ['eudemons', 'combo'],
+    'caseStyle': 'c',
+    'color': 'c'
+  },
+  17: {
+    'words': ['electron', 'shower', 'night'],
+    'caseStyle': 'c',
+    'color': 'c'
+  },
+  18: {
+    'words': ['computerphobe', 'user'],
+    'caseStyle': 'c',
+    'color': 'c'
+  },
+  19: {
+    'words': ['quantum', 'daemon'],
+    'caseStyle': 'c',
+    'color': 'c'
+  },
+  20: {
+    'words': ['has', 'not', 'paid'],
+    'caseStyle': 'c',
+    'color': 'c'
+  }
+}
+
+let words_specs_full = {
+  1: {
+    'words': ['telotaxi', 'is', 'sour'],
+    'caseStyle': 'kebab',
+    'color': 'mono'
+  },
+  2: {
+    'words': ['vinyl', 'paint'],
+    'caseStyle': 'kebab',
+    'color': 'mono'
+  }.
+  3: {
+    'words': ['handy', 'dandy'],
+    'caseStyle': 'kebab',
+    'color': 'mono'
+  },
+  4: {
+    'words': ['file', 'not', 'found'],
+    'caseStyle': 'kebab',
+    'color': 'mono'
+  },
+  5: {
+    'words': ['brew', 'coffee', 'now'],
+    'caseStyle': 'kebab',
+    'color': 'mono'
+  }
+  6: {
+    'words': ['asphyxy', 'cloud'],
+    'caseStyle': 'kebab',
+    'color': 'color'
+  },
+  7: {
+    'words': ['narrow', 'polyphone'],
+    'caseStyle': 'kebab',
+    'color': 'color'
+  },
+  8: {
+    'words': ['dark', 'side', 'guy'],
+    'caseStyle': 'kebab',
+    'color': 'color'
+  },
+  9: {
+    'words': ['biotron', 'mnemonic'],
+    'caseStyle': 'kebab',
+    'color': 'color'
+  },
+  10: {
+    'words': ['modal', 'horizontal', 'size'],
+    'caseStyle': 'kebab',
+    'color': 'color'
+  },
+  11: {
+    'words': ['sad', 'user'],
+    'caseStyle': 'camel',
+    'color': 'mono'
+  },
+  12: {
+    'words': ['come', 'closer'],
+    'caseStyle': 'camel',
+    'color': 'mono'
+  },
+  13: {
+    'words': ['rainbow', 'kid'],
+    'caseStyle': 'camel',
+    'color': 'mono'
+  },
+  14: {
+    'words': ['has', 'golden', 'pass'],
+    'caseStyle': 'camel',
+    'color': 'mono'
+  },
+  15: {
+    'words': ['megatron', 'movies'],
+    'caseStyle': 'camel',
+    'color': 'mono'
+  },
+  16: {
+    'words': ['eudemons', 'combo'],
+    'caseStyle': 'camel',
+    'color': 'color'
+  },
+  17: {
+    'words': ['electron', 'shower', 'night'],
+    'caseStyle': 'camel',
+    'color': 'color'
+  },
+  18: {
+    'words': ['computerphobe', 'user'],
+    'caseStyle': 'camel',
+    'color': 'color'
+  },
+  19: {
+    'words': ['quantum', 'daemon'],
+    'caseStyle': 'camel',
+    'color': 'color'
+  },
+  20: {
+    'words': ['has', 'not', 'paid'],
+    'caseStyle': 'camel',
+    'color': 'color'
+  }
+}
